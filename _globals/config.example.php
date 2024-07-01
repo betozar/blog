870cdaf861af2a89ec29934721f11e7ddab25027
betozar <monrazdot@gmail.com>
2024-07-01 15:00:56 -0600

    fix basepath

--- _globals/config.example.php
+++ _globals/config.example.php
@@ -6,7 +6,7 @@ define('APP_URL', 'http://blog.test');
 define('APP_TIMEZONE', 'America/Mexico_City');
 define('APP_LANG', 'en');
 
-define('BASE_PATH', __DIR__);
+define('BASE_PATH', __DIR__.'/..');
 define('DB_PATH', BASE_PATH.'/_database');
 define('FUNC_PATH', BASE_PATH.'/_functions');
 define('LAYOUT_PATH', BASE_PATH.'/_layout');
